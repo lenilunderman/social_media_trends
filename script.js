f9ddd01dc793935736ca71e0476e988dab5e3df3
Leni Lunderman <lenilunderman@gmail.com>
2021-02-02 18:24:25 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const addUser = document.getElementById('add-users-btn')
+const multipleUser = document.getElementById('multipleUser-btn')
+const searchUser = document.getElementById('search-btn')
+const calculateUser = document.getElementById('calculateUser-btn')
+
+
+console.log(addUser);
+console.log(multipleUser);
+console.log(searchUser);
+console.log(calculateUser);
